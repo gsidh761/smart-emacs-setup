037bc2b67baa099ac93c445735eb21ca3826a79b
gsidh761 <gsidh761@mtroyal.ca>
2025-09-15 15:31:08 -0600

    Uses curl instead of wget in the setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mkdir backup
 echo "Backing up old init.el file..."
 cp ~/.emacs.d/init.el ./backup/
 echo "Downloading new init.el file..."
-wget https://raw.githubusercontent.com/gsidh761/smart-emacs-setup/refs/heads/main/init.el
+curl https://raw.githubusercontent.com/gsidh761/smart-emacs-setup/refs/heads/main/init.el --output init.el
 echo "Copying new init.el file to ~/.emacs.d/..."
 cp init.el ~/.emacs.d/ 
 rm init.el
