04ed6670275487be35401055d1ed48f60f568ffd
gsidh761 <gsidh761@mtroyal.ca>
2025-09-15 10:52:38 -0600

    Adds new themes and features in the init.el file

--- init.el
+++ init.el
@@ -1,60 +1,47 @@
-;; Setup environment
-(setq inhibit-startup-message t)
+;; ===============================
+;; Smart Emacs Setup (init.el)
+;; ===============================
+
+;; Basic environment setup
+(setq inhibit-startup-message t
+      visible-bell t
+      tab-width 4
+      truncate-lines t)
+
 (tooltip-mode -1)
 (menu-bar-mode -1)
-(setq visible-bell t)
 (recentf-mode 1)
 (save-place-mode 1)
-(setq tab-width 4)
 
-(require 'package)
-
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("gnu-devel" . "https://elpa.gnu.org/devel/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "https://melpa.org/packages/")))
-
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(load "cpp-auto-include.el")
-;; (add-hook 'emacs-lisp-mode-hook 'auto-make-header)
-
-;; Turn on company mode
-(global-company-mode t)
-(add-hook 'after-init-hook 'global-company-mode)
-(setq package-archives
-      '(("elpy" . "http://jorgenschaefer.github.io/packages/")
-        ("melpa" . "https://melpa.org/packages/")
-        ("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")))
-
-(unless (fboundp 'package-activate-all) (package-initialize))
-
-;; Turn on line and column numbers globally
+;; Line and column numbers
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-;; Turn off line numbers in specific modes
-(defun disable-line-numbers()
+(defun disable-line-numbers ()
   (display-line-numbers-mode 0))
-
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook))
+(dolist (mode '(org-mode-hook term-mode-hook eshell-mode-hook))
   (add-hook mode #'disable-line-numbers))
 
-;; global key bindings
+;; Global key bindings
 (global-set-key [select] 'end-of-line)
 (global-set-key "\eOn" ".")
 (global-set-key (kbd "C-M-r") 'recentf-open-files)
 
-;; setup packages
+;; Package setup
 (require 'package)
+(setq package-archives
+      '(("gnu"          . "https://elpa.gnu.org/packages/")
+        ("gnu-devel"    . "https://elpa.gnu.org/devel/")
+        ("nongnu"       . "https://elpa.nongnu.org/nongnu/")
+        ("melpa"        . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("elpy"         . "http://jorgenschaefer.github.io/packages/")))
 (package-initialize)
 
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Ivy, Counsel, Swiper
 (use-package ivy
   :diminish ivy-mode
   :bind (("C-s" . swiper)
@@ -79,94 +66,88 @@
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-		 ("C-x b" . counsel-ibuffer)
-		 ("C-x C-f" . counsel-find-file)
-		 :map minibuffer-local-map
-		 ("C-r" . 'counsel-minibuffer-history))
-  :config (setq ivy-initial-inputs-alist nil)) ;; do not start search with ^
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :config (setq ivy-initial-inputs-alist nil))
 
+;; Which-key
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config (setq which-key-idle-delay 0.3))
 
-(use-package doom-themes
-;;  :init (load-theme 'doom-dark+ t) ;; use doom-* themes to ensure minibuffers have correct style
-)
-
+;; Doom themes & modeline
+(use-package doom-themes)
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
-(setq doom-modeline-icon nil) ;; turns off doom mode icons
+(setq doom-modeline-icon nil)
 
-;;for c++ compiles
-(defun cplusplus-compile-command(comm)
+;; Company (global auto-complete)
+(use-package company
+  :init (global-company-mode))
+
+;; Auto C++ header includes
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'cpp-auto-include)
+
+;; Compilation helpers
+(defun cplusplus-compile-command (comm)
   (make-local-variable 'compile-command)
   (if (or (file-exists-p "makefile")
-           (file-exists-p "Makefile"))
-       (setq compile-command "make -k all ")
-       (setq compile-command
-         (concat comm " "
-                   (file-name-nondirectory buffer-file-truename)
-                   " -pedantic-errors -Wall -Wconversion -o "
-                   (file-name-sans-extension (file-name-nondirectory buffer-file-truename) )))))
+          (file-exists-p "Makefile"))
+      (setq compile-command "make -k all ")
+    (setq compile-command
+          (concat comm " "
+                  (file-name-nondirectory buffer-file-truename)
+                  " -pedantic-errors -Wall -Wconversion -o "
+                  (file-name-sans-extension (file-name-nondirectory buffer-file-truename)))))))
 
-;;for java compile
-(defun java-compile-command()
+(defun java-compile-command ()
   (make-local-variable 'compile-command)
-  (setq compile-command (concat "javac " (file-name-nondirectory buffer-file-truename)))
-)
+  (setq compile-command (concat "javac " (file-name-nondirectory buffer-file-truename))))
 
-;;for php lint
-(defun php-compile-command()
+(defun php-compile-command ()
   (make-local-variable 'compile-command)
-  (setq compile-command (concat "php -l " (file-name-nondirectory buffer-file-truename)))
-)
+  (setq compile-command (concat "php -l " (file-name-nondirectory buffer-file-truename))))
 
-(add-hook 'c++-mode-hook (lambda() (cplusplus-compile-command "g++")))
-(add-hook 'c-mode-hook (lambda() (cplusplus-compile-command "gcc")))
-(add-hook 'asm-mode-hook (lambda() (cplusplus-compile-command "gcc")))
-(add-hook 'java-mode-hook (lambda() (java-compile-command)))
-(add-hook 'php-mode-hook (lambda() (php-compile-command)))
+(add-hook 'c++-mode-hook (lambda () (cplusplus-compile-command "g++")))
+(add-hook 'c-mode-hook  (lambda () (cplusplus-compile-command "gcc")))
+(add-hook 'asm-mode-hook (lambda () (cplusplus-compile-command "gcc")))
+(add-hook 'java-mode-hook 'java-compile-command)
+(add-hook 'php-mode-hook 'php-compile-command)
 
+;; Detect .h files correctly
 (defun c-c++-header ()
   (interactive)
-  (let ((c-file (concat (substring (buffer-file-name) 0 -1) "c")))
-    (if (file-exists-p c-file)
-        (c-mode)
-      (c++-mode))))
+  (let ((c-file (concat (substring (buffer-file-name) 0 -1) "c"))))
+    (if (file-exists-p c-file) (c-mode) (c++-mode))))
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c-c++-header))
 
+;; Custom C style
 (defun my-c-style ()
-  "loads my custom c-style."
+  "Load custom C style."
   (c-set-style "stroustrup")
   (c-set-offset 'inline-open '0)
   (setq c-indent-comments-syntactically-p t
-	indent-tabs-mode nil
+        indent-tabs-mode nil
         case-fold-search nil
         c-recognize-knr-p nil
         c-auto-newline nil))
 (add-hook 'c-mode-common-hook 'my-c-style)
 
-;; customize compilation buffer
+;; Colorize compilation buffer
 (require 'ansi-color)
 (defun colorize-compilation-buffer ()
   (let ((inhibit-read-only t))
     (ansi-color-apply-on-region (point-min) (point-max))))
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
-
+;; Custom variables (auto-generated)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
  '(package-selected-packages
-   '(header2 cpp-auto-include counsel which-key ivy-rich ivy use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '(header2 cpp-auto-include counsel which-key ivy-rich ivy use-package company doom-themes doom-modeline)))
+(custom-set-faces)
